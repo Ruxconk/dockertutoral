1b57f29ef49c4f451dbd15a2dc7bd4e0e96f9644
ruxconk <ruxconk@gmail.com>
2019-02-18 14:54:37 +0000

    JIG-666 Changed directory structure and adding a new file to remove untagged images

--- python-server-2.7.15.py
+++
@@ -1,9 +0,0 @@
-# only works with Python 2
-import SimpleHTTPServer
-import SocketServer
-
-PORT = 9000
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-httpd = SocketServer.TCPServer(("", PORT), Handler)
-httpd.serve_forever()
-
--- python-server-3.6.8.py
+++
@@ -1,9 +0,0 @@
-# only works with Python 3
-import http.server
-import socketserver
-
-PORT = 9000
-Handler = http.server.SimpleHTTPRequestHandler
-httpd = socketserver.TCPServer(("", PORT), Handler)
-print("serving at port", PORT)
-httpd.serve_forever()
